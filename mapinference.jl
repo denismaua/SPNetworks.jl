3b56de35133487b8073b37b9a0014f03c2810b17
Denis Maua <denis.maua@gmail.com>
2020-08-12 20:58:11 -0300

    moved bp inf into function

--- mapinference.jl
+++ mapinference.jl
@@ -1,13 +1,13 @@
 # Runs MAP Inference algorithms
 using SumProductNetworks
 import SumProductNetworks: leaves, isleaf, issum, isprod, IndicatorFunction
-import SumProductNetworks.MAP: maxproduct!, spn2bn
+import SumProductNetworks.MAP: maxproduct!, beliefpropagation! #, spn2bn
 import GraphicalModels: FactorGraph, FGNode, VariableNode, FactorNode
-import GraphicalModels.MessagePassing: HybridBeliefPropagation, update!, decode, marginal, setevidence!, setmapvar!
+# import GraphicalModels.MessagePassing: HybridBeliefPropagation, update!, decode, marginal, setevidence!, setmapvar!, rmevidence!
 
 # Maximum number of iterations of belief propagation
 # const maxiterations = 10
-maxiterations = 10
+maxiterations = 5
 
 # spn_filename = ARGS[1]
 # spn_filename = "/Users/denis/code/SumProductNetworks/assets/example.pyspn.spn"
@@ -16,9 +16,9 @@ maxiterations = 10
 # query_filename = "/Users/denis/learned-spns/spambase/spambase.query"
 # evid_filename = "/Users/denis/learned-spns/spambase/spambase.evid"
 # nltcs
-# spn_filename = "/Users/denis/learned-spns/nltcs/nltcs.spn2"
-# query_filename = "/Users/denis/learned-spns/nltcs/nltcs.query"
-# evid_filename = "/Users/denis/learned-spns/nltcs/nltcs.evid"
+spn_filename = "/Users/denis/learned-spns/nltcs/nltcs.spn2"
+query_filename = "/Users/denis/learned-spns/nltcs/nltcs.query"
+evid_filename = "/Users/denis/learned-spns/nltcs/nltcs.evid"
 # hepatitis
 # spn_filename = "/Users/denis/learned-spns/hepatitis/hepatitis.spn2"
 # query_filename = "/Users/denis/learned-spns/hepatitis/hepatitis.query"
@@ -36,9 +36,9 @@ maxiterations = 10
 # query_filename = "/Users/denis/learned-spns/dna/dna.query"
 # evid_filename = "/Users/denis/learned-spns/dna/dna.evid"
 # nips
-spn_filename = "/Users/denis/learned-spns/nips/nips.spn2"
-query_filename = "/Users/denis/learned-spns/nips/nips.query"
-evid_filename = "/Users/denis/learned-spns/nips/nips.evid"
+# spn_filename = "/Users/denis/learned-spns/nips/nips.spn2"
+# query_filename = "/Users/denis/learned-spns/nips/nips.query"
+# evid_filename = "/Users/denis/learned-spns/nips/nips.evid"
 
 println("SPN: ", spn_filename)
 
@@ -86,93 +86,112 @@ mpvalue = spn(x)
 printstyled("\nMaxProduct: "; color = :green)
 println("$mpvalue [$(mptime)s]\n")
 
+# Run hybrid belief propagation
+bptime = @elapsed beliefpropagation!(x, spn, query; maxiterations = 3, lowerbound = true, rndminit = false)
+bpvalue = spn(x)
+printstyled("\nBeliefPropagation: "; color = :green)
+println("$bpvalue [$(bptime)s]\n")
+
+printstyled("Best Ratio: "; color = :green)
+println( bpvalue/mpvalue )
+
 # Translate SPN into distribution-equivalent Factor Graph
-println("Generating factor graph...")
-@time fg = spn2bn(spn)
+# println("Generating factor graph...")
+# @time fg = spn2bn(spn)
 
-# consistency checks
-@assert length(fg.variables) == (length(spn) + nvars)
-@assert length(fg.factors) == length(spn)
+# # consistency checks
+# @assert length(fg.variables) == (length(spn) + nvars)
+# @assert length(fg.factors) == length(spn)
 
-# Initialize belief propagation
-bp = HybridBeliefPropagation(fg; rndinit = false) # rndinit = true generates random initial message; = true sets all to 1.
-bp.normalize = true # normalize messages (sum = 1)?
-# Set evidence and query
-setevidence!(bp, "Y1", 2)
-for (v,e) in evidence
-    setevidence!(bp, "X$v", e)
-end
-for v in query
-    setmapvar!(bp, "X$v")
-end
-# run Inference
-cpad(s, n::Integer, p=" ") = rpad(lpad(s,div(n+length(s),2),p),n,p)
-columns = ["it", "time (s)", "residual", "value", "best?"]
-widths = [4, 10, 10, 20, 7]
-fmts = [" %2d ", " %8.2f ", " %8.2f ", " %18.16f ", " %7s "]
-bordercolor = :light_cyan
-headercolor = :cyan
-fieldcolor = :normal
-printstyled('╭', join(map(w -> repeat('─',w), widths), '┬'), '╮', '\n' ;color = bordercolor)
-for (col,w) in zip(columns,widths)
-    printstyled("│"; color = bordercolor)
-    printstyled(cpad(col, w); color = headercolor)
-end
-printstyled("│\n"; color = bordercolor)
-printstyled('├', join(map(w -> repeat('─',w), widths), '┼'), '┤', '\n' ;color = bordercolor)
-# value of best incumbent solution
-best = -Inf
-start = time_ns()
-for it=1:maxiterations
-    # res = update!(bp)  
-    # downward propagation  
-    res = 0.0
-    for i = length(spn):-1:1
-        node = spn[i]
-        # compute incoming messages from children
-        if isleaf(node) 
-            factor = fg.factors[string(i)]           
-            update!(bp, fg.variables["X$(node.scope)"], factor)
-            res = max(res, update!(bp, factor, fg.variables["Y$i"]))
-        else
-            factor = fg.factors[string(i)]
-            for j in node.children
-                update!(bp, fg.variables["Y$j"], factor)
-            end
-            res = max(res, update!(bp, factor, fg.variables["Y$i"]))
-        end
-    end
-    # upward propagation
-    for i = 1:length(spn)
-        node = spn[i]
-        # compute outgoing messages to children
-        if isleaf(node)
-            factor = fg.factors[string(i)]
-            update!(bp, fg.variables["Y$i"], factor)
-            res = max(res, update!(bp, factor, fg.variables["X$(node.scope)"]))
-        else
-            factor = fg.factors[string(i)]
-            update!(bp, fg.variables["Y$i"], factor)
-            for j in node.children
-                res = max(res, update!(bp, factor, fg.variables["Y$j"]))
-            end
-        end
-    end
-    etime = (time_ns()-start)/1e9;
-    # prob = marginal(bp,"Y1")[2]
-    for i in query
-        x[i] = decode(bp, "X$i")
-    end
-    prob = spn(x)
-    for (col,w) in zip([it, round(etime, digits=2), round(res,digits=2), round(prob, digits=16), prob >= best ? "*" : " "],widths)
-        printstyled("│"; color = bordercolor)
-        printstyled(lpad(col, w-1), ' '; color = fieldcolor)
-    end 
-    printstyled("│\n"; color = bordercolor)
-    global best = max(best, prob)
-    if res < 0.001 break end # early stop
-end
-printstyled('╰', join(map(w -> repeat('─',w), widths), '┴'), '╯', '\n' ;color = bordercolor)
+# # Initialize belief propagation
+# bp = HybridBeliefPropagation(fg; rndinit = false) # rndinit = true generates random initial message; = true sets all to 1.
+# bp.normalize = true # normalize messages (sum = 1)?
+# # Set evidence and query
+# setevidence!(bp, "Y1", 2)
+# for (v,e) in evidence
+#     setevidence!(bp, "X$v", e)
+# end
+# for v in query
+#     setmapvar!(bp, "X$v")
+# end
+# # run Inference
+# println("Running belief propagation for $maxiterations iterations")
+# cpad(s, n::Integer, p=" ") = rpad(lpad(s,div(n+length(s),2),p),n,p) # for printing centered
+# columns = ["it", "time (s)", "residual", "value", "best?"]
+# widths = [4, 10, 10, 24, 7]
+# bordercolor = :light_cyan
+# headercolor = :cyan
+# fieldcolor = :normal
+# printstyled('╭', join(map(w -> repeat('─',w), widths), '┬'), '╮', '\n' ;color = bordercolor)
+# for (col,w) in zip(columns,widths)
+#     printstyled("│"; color = bordercolor)
+#     printstyled(cpad(col, w); color = headercolor)
+# end
+# printstyled("│\n"; color = bordercolor)
+# printstyled('├', join(map(w -> repeat('─',w), widths), '┼'), '┤', '\n' ;color = bordercolor)
+# # value of best incumbent solution
+# # best = -Inf
+# best = mpvalue
+# start = time_ns()
+# for it=1:maxiterations
+#     # if it == 1
+#     #     for v in query
+#     #         setevidence!(bp, "X$v", x[v])
+#     #     end
+#     # elseif it == 2
+#     #     for v in query
+#     #         rmevidence!(bp, "X$v")
+#     #     end
+#     # end
+#     # downward propagation  
+#     res = 0.0
+#     for i = length(spn):-1:1
+#         node = spn[i]
+#         # compute incoming messages from children
+#         if isleaf(node) 
+#             factor = fg.factors[string(i)]           
+#             update!(bp, fg.variables["X$(node.scope)"], factor)
+#             res = max(res, update!(bp, factor, fg.variables["Y$i"]))
+#         else
+#             factor = fg.factors[string(i)]
+#             for j in node.children
+#                 update!(bp, fg.variables["Y$j"], factor)
+#             end
+#             res = max(res, update!(bp, factor, fg.variables["Y$i"]))
+#         end
+#     end
+#     # upward propagation
+#     for i = 1:length(spn)
+#         node = spn[i]
+#         # compute outgoing messages to children
+#         if isleaf(node)
+#             factor = fg.factors[string(i)]
+#             update!(bp, fg.variables["Y$i"], factor)
+#             res = max(res, update!(bp, factor, fg.variables["X$(node.scope)"]))
+#         else
+#             factor = fg.factors[string(i)]
+#             update!(bp, fg.variables["Y$i"], factor)
+#             for j in node.children
+#                 res = max(res, update!(bp, factor, fg.variables["Y$j"]))
+#             end
+#         end
+#     end
+#     # res = update!(bp)  
+#     etime = (time_ns()-start)/1e9;
+#     # prob = marginal(bp,"Y1")[2]
+#     for i in query
+#         x[i] = decode(bp, "X$i")
+#     end
+#     prob = spn(x)
+#     for (col,w) in zip([it, round(etime, digits=2), round(res,digits=2), prob, prob >= best ? "*" : " "],widths)
+#         printstyled("│"; color = bordercolor)
+#         printstyled(lpad(col, w-1), ' '; color = fieldcolor)
+#     end 
+#     printstyled("│\n"; color = bordercolor)
+#     global best = max(best, prob)
+#     if res < 0.001 break end # early stop
+# end
+# printstyled('╰', join(map(w -> repeat('─',w), widths), '┴'), '╯', '\n' ;color = bordercolor)
 
-printstyled("Ratio: "; color = :green)
-println( best/mpvalue )
+# printstyled("Best Ratio: "; color = :green)
+# println( best/mpvalue )
---
+++ src/Inference/MAP/BeliefPropagation.jl
@@ -0,0 +1,200 @@
+# Translates SPN to Bayesian Networks and runs Hybrid-Product Belief Propagation to obtain MAP configuration
+import GraphicalModels: FactorGraph, FGNode, VariableNode, FactorNode
+import GraphicalModels.MessagePassing: HybridBeliefPropagation, update!, decode, setevidence!, setmapvar!
+
+"""
+    spn2bn(spn::SumProductNetwork)
+
+Returns factor graph representing the computation graph of `spn`.
+"""
+function spn2bn(spn::SumProductNetwork)
+    # recover dimensionality for each variable
+    vdims = SumProductNetworks.vardims(spn)
+    variables = Dict{String,VariableNode}()
+    # sizehint!(variables,length(vdims)+length(spn))
+    factors = Dict{String,FactorNode}()
+    # sizehint!(factors,length(spn))
+    # Add root nodes for manifest variables
+    for (v,d) in vdims
+        variables["X" * string(v)] = VariableNode(d)
+    end
+    for i = length(spn):-1:1
+        node = spn[i]
+        # println(i, " ", node)
+        if issum(node)
+            # process sum node
+            var = VariableNode(2)
+            variables["Y"*string(i)] = var
+            factor = FactorNode(
+                Array{Float64}(undef, Tuple(2 for _ = 1:(length(node.children)+1)) ), # factor
+                VariableNode[var ; map(j -> variables["Y"*string(j)], node.children)] # neighbors
+                )
+            # P(Y=2|parents=z) = sum( node.weights[i] | z[i] = 2 )
+            for z in CartesianIndices(Tuple(2 for _ = 1:length(node.children)))
+                prob = min(1.0,mapreduce(j -> node.weights[j]*(z[j]-1), +, 1:length(z)))
+                factor.factor[2,z] = log(prob)
+                factor.factor[1,z] = log(1 - prob)
+            end
+            factors[string(i)] = factor
+            # display(factor.neighbors)
+        elseif isprod(node)
+            # process product node
+            var = VariableNode(2)
+            variables["Y"*string(i)] = var
+            factor = FactorNode(
+                zeros(Float64, Tuple(2 for _ = 1:(length(node.children)+1)) ), # factor
+                VariableNode[var ; map(j -> variables["Y"*string(j)], node.children)] # neighbors
+                )
+            # P(Y=2|parents) = { 1 if all parents = 1, 0 otherwise }
+            factor.factor[2:2:(end-1)] .= -Inf
+            factor.factor[end-1] = -Inf
+            factors[string(i)] = factor
+        elseif isa(node, IndicatorFunction)
+            # process leaf node
+            var = VariableNode(2)
+            variables["Y"*string(i)] = var
+            parent = variables["X"*string(node.scope)]
+            factor = FactorNode(zeros(Float64, 2, vdims[node.scope]), [var, parent])
+            # P(Y=2|parent) = { 1 if parent = node.value, 0 otherwise }
+            factor.factor[2:2:end] .= -Inf
+            factor.factor[1,convert(Int,node.value)] = -Inf
+            factor.factor[2,convert(Int,node.value)] = 0.0 
+            factors[string(i)] = factor
+        else
+            @error "Unsupported node type: $(typeof(node))"
+        end
+    end
+    # Create and return factor graph
+    FactorGraph(variables, factors)
+end
+
+"""
+    beliefpropagation!(x, spn, query; kwargs)
+
+Runs mixed belief propagation algorithm with given `spn`, `query` variables and evidence `x`.
+
+### Keyword Arguments
+- `maxiterations`: positive integer value specifying maximum number of iterations [default: `10`]
+- `verbose`: whether to output information during inference run [default: `true`]
+- `normalize`: whether to normalize messages [defulat: `true`]
+- `lowerbound`: whether to use the probability of `x` as initial lower bound [default: `false`]
+- `earlystop`: early stop algorithm if residual is below that threshold [default: `0.001`]
+- `rndminit`: whether to use random initialization of messages (rather than constant initialization) [default: `false`]
+"""
+function beliefpropagation!(x::AbstractArray{<:Real}, spn::SumProductNetwork, query; maxiterations = 10, verbose = true, normalize = true, lowerbound = false, earlystop = 0.001, rndminit = false)
+    # Translate SPN into distribution-equivalent Factor Graph
+    fg, t, bytes, gctime, mallocs = @timed spn2bn(spn)
+    if verbose 
+        @info "Generated factor graph ($(round(bytes/1048576,digits=1))MiB) in $t secs."
+    end
+    # fg = spn2bn(spn) 
+    # consistency checks
+    @assert length(fg.variables) == (length(spn) + length(scope(spn)))
+    @assert length(fg.factors) == length(spn)
+
+    # Initialize belief propagation
+    bp = HybridBeliefPropagation(fg; rndinit = rndminit) # rndinit = true generates random initial message; = true sets all to 1.
+    bp.normalize = normalize # normalize messages (sum = 1)?
+    # Set evidence and query
+    setevidence!(bp, "Y1", 2)
+    for v in findall(isfinite,x) # filter non-marginalized variables
+        if v ∉ query # filter out query variables
+            setevidence!(bp, "X$v", x[v]) # remaining are evidence
+        end
+    end
+    for v in query
+        setmapvar!(bp, "X$v")
+    end
+    # run Inference
+    if verbose
+        println("Running belief propagation for $maxiterations iterations")
+    end
+    cpad(s, n::Integer, p=" ") = rpad(lpad(s,div(n+length(s),2),p),n,p) # for printing centered
+    columns = ["it", "time (s)", "residual", "value", "best?"]
+    widths = [4, 10, 10, 24, 7]
+    bordercolor = :light_cyan
+    headercolor = :cyan
+    fieldcolor = :normal
+    if verbose
+        printstyled('╭', join(map(w -> repeat('─',w), widths), '┬'), '╮', '\n' ;color = bordercolor)
+        for (col,w) in zip(columns,widths)
+            printstyled("│"; color = bordercolor)
+            printstyled(cpad(col, w); color = headercolor)
+        end
+        printstyled("│\n"; color = bordercolor)
+        printstyled('├', join(map(w -> repeat('─',w), widths), '┼'), '┤', '\n' ;color = bordercolor)
+    end
+    # value of best incumbent solution
+    best = -Inf
+    if lowerbound
+        best = spn(x)
+    end
+    y = copy(x) # incumbent solution
+    start = time_ns()
+    for it=1:maxiterations
+        # if it == 1
+        #     for v in query
+        #         setevidence!(bp, "X$v", x[v])
+        #     end
+        # elseif it == 2
+        #     for v in query
+        #         rmevidence!(bp, "X$v")
+        #     end
+        # end
+        # downward propagation  
+        res = 0.0
+        for i = length(spn):-1:1
+            node = spn[i]
+            # compute incoming messages from children
+            if isleaf(node) 
+                factor = fg.factors[string(i)]           
+                update!(bp, fg.variables["X$(node.scope)"], factor)
+                res = max(res, update!(bp, factor, fg.variables["Y$i"]))
+            else
+                factor = fg.factors[string(i)]
+                for j in node.children
+                    update!(bp, fg.variables["Y$j"], factor)
+                end
+                res = max(res, update!(bp, factor, fg.variables["Y$i"]))
+            end
+        end
+        # upward propagation
+        for i = 1:length(spn)
+            node = spn[i]
+            # compute outgoing messages to children
+            if isleaf(node)
+                factor = fg.factors[string(i)]
+                update!(bp, fg.variables["Y$i"], factor)
+                res = max(res, update!(bp, factor, fg.variables["X$(node.scope)"]))
+            else
+                factor = fg.factors[string(i)]
+                update!(bp, fg.variables["Y$i"], factor)
+                for j in node.children
+                    res = max(res, update!(bp, factor, fg.variables["Y$j"]))
+                end
+            end
+        end
+        for i in query
+            y[i] = decode(bp, "X$i")
+        end
+        prob = spn(y)
+        if verbose
+            etime = (time_ns()-start)/1e9;
+            for (col,w) in zip([it, round(etime, digits=2), round(res,digits=2), prob, prob >= best ? "*" : " "],widths)
+                printstyled("│"; color = bordercolor)
+                printstyled(lpad(col, w-1), ' '; color = fieldcolor)
+            end 
+            printstyled("│\n"; color = bordercolor)
+        end
+        if prob > best
+            best = prob
+            x .= y
+        end
+        # best = max(best, prob)
+        if res < earlystop break end # early stop
+    end
+    if verbose
+        printstyled('╰', join(map(w -> repeat('─',w), widths), '┴'), '╯', '\n' ;color = bordercolor)
+    end
+    best
+end
